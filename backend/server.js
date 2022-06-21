06559a8fddde07677208d2f297a0cd32bebb6014
Rakibul71 <rakibulislam202019@gmail.com>
2022-06-21 14:04:53 +0600

    connecting front and back together running

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dotEnv = require('dotenv');
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send("server is running ")
+})
+
+
+dotEnv.config({
+    path: 'backend/config/config.env'
+})
+
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, (error) => {
+    if (!error) {
+        console.log(`server is running port ${PORT}`);
+    }
+})
